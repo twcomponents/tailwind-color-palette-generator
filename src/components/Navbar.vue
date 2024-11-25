955aaa26c4234c7003055f9cf1eb81ccd2c019ac
Eyüp Erdoğan <epsilonelittr@gmail.com>
2024-11-25 12:26:42 +0300

    Improve navbar styling

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -4,7 +4,7 @@
 
 <template>
   <div
-    class="fixed top-0 left-0 py-4 w-full dark:bg-zinc-900/60 border-b dark:border-zinc-800 z-50 backdrop-blur-md"
+    class="fixed top-0 left-0 py-4 w-full bg-white/80 dark:bg-zinc-900/80 border-b dark:border-zinc-800 z-50 backdrop-blur-md"
   >
     <nav class="flex items-center justify-between default-container">
       <div class="font-extrabold text-3xl">
