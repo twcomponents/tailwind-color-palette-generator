24c62da607c8df1b9f7aa2344a29fa1121164b10
Ümit Ahmed <135972028+umitahmedd@users.noreply.github.com>
2024-11-26 14:23:16 +0300

    Add VueUse core and motion plugins

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,11 @@ import './style.scss';
 import App from './App.vue';
 import router from './router';
 import Vue3ColorPicker from 'vue3-colorpicker';
+import { MotionPlugin } from '@vueuse/motion';
 import 'vue3-colorpicker/style.css';
 
 const app = createApp(App);
 app.use(router);
 app.use(Vue3ColorPicker);
+app.use(MotionPlugin);
 app.mount('#app');
