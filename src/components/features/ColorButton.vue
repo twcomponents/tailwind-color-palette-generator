4b2da60d0771bc1cc76fc46d46214fdf1fb3b1b6
Eyüp <epsilonelittr@gmail.com>
2024-11-27 20:18:18 +0300

    Add copy palette color feature

--- src/components/features/ColorButton.vue
+++ src/components/features/ColorButton.vue
@@ -1,22 +1,43 @@
 <template>
-  <button
-    :class="[
-      `bg-${props.themeVariableKey}-${paletteColor.level}`,
-      paletteColor.level >= 50 && paletteColor.level <= 300 && 'text-zinc-700',
-      paletteColor.level >= 400 && paletteColor.level <= 950 && 'text-zinc-50',
-    ]"
-  >
-    <span>
-      {{ paletteColor.level }}
-    </span>
+  <div class="flex flex-col gap-2 items-center">
+    <!-- Color Button -->
+    <button
+      :class="[
+        `bg-${props.themeVariableKey}-${paletteColor.level}`,
+        paletteColor.level >= 50 &&
+          paletteColor.level <= 300 &&
+          'text-zinc-700',
+        paletteColor.level >= 400 &&
+          paletteColor.level <= 950 &&
+          'text-zinc-50',
+      ]"
+      @mouseenter="isCopyHintVisible = true"
+      @mouseleave="isCopyHintVisible = false"
+      @click="onCopyClick()"
+    >
+      <span>
+        {{ paletteColor.level }}
+      </span>
 
-    <span>
-      {{ paletteColor.color }}
+      <span>
+        {{ paletteColor.color }}
+      </span>
+    </button>
+
+    <!-- Copy Hint -->
+    <span
+      class="text-xs select-none"
+      :class="{ 'opacity-0': !isCopyHintVisible }"
+    >
+      {{ copyHintText }}
     </span>
-  </button>
+  </div>
 </template>
 
 <script setup lang="ts">
+  // native
+  import { ref } from 'vue';
+
   // models
   import {
     IPaletteColor,
@@ -27,4 +48,18 @@
     paletteColor: IPaletteColor;
     themeVariableKey: ColorVariableTheme;
   }>();
+
+  const isCopyHintVisible = ref(false);
+  const copyHintText = ref('Copy');
+
+  const onCopyClick = () => {
+    navigator.clipboard.writeText(props.paletteColor.color);
+    isCopyHintVisible.value = true;
+    copyHintText.value = 'Copied 🤘';
+
+    setTimeout(() => {
+      copyHintText.value = 'Copy';
+      isCopyHintVisible.value = false;
+    }, 500);
+  };
 </script>
