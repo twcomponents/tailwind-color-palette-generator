19d528df45aba68c0db159a1e1f4247a429b964b
Eyüp <epsilonelittr@gmail.com>
2024-11-27 21:54:24 +0300

    Add export modal

--- src/components/features/ColorPalette.vue
+++ src/components/features/ColorPalette.vue
@@ -1,4 +1,5 @@
 <template>
+  <!-- Palette -->
   <div class="flex flex-col gap-3 container w-7/12">
     <!-- Header -->
     <div class="flex flex-row gap-2 items-center justify-between px-2">
@@ -17,12 +18,29 @@
 
       <!-- Right -->
       <div class="flex flex-row gap-2 items-center">
-        <button
-          class="dark:text-zinc-400 dark:hover:text-zinc-300 transition-all duration-200 ease-in-out"
-          @click="onExportClick()"
-        >
-          Export
-        </button>
+        <AlertDialog>
+          <AlertDialogTrigger as-child>
+            <button
+              class="dark:text-zinc-400 dark:hover:text-zinc-300 transition-all duration-200 ease-in-out"
+              @click="onExportClick()"
+            >
+              Export
+            </button>
+          </AlertDialogTrigger>
+          <AlertDialogContent>
+            <AlertDialogHeader>
+              <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+              <AlertDialogDescription>
+                This action cannot be undone. This will permanently delete your
+                account and remove your data from our servers.
+              </AlertDialogDescription>
+            </AlertDialogHeader>
+            <AlertDialogFooter>
+              <AlertDialogCancel>Cancel</AlertDialogCancel>
+              <AlertDialogAction>Continue</AlertDialogAction>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialog>
       </div>
     </div>
 
@@ -47,6 +65,19 @@
 
   // models
   import { IPaletteColor } from '@/shared/models/color.model';
+
+  // shadcn
+  import {
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogCancel,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+    AlertDialogTrigger,
+  } from '@/components/ui/alert-dialog';
 
   const props = defineProps<{
     colorPalette: IPaletteColor[];
