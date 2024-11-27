b6b10eae4d0368a6c2ccfa02972580e96e756719
Eyüp Erdoğan <epsilonelittr@gmail.com>
2024-11-27 17:37:41 +0300

    Add home page link

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -4,5 +4,12 @@
   >
     <h1 class="text-3xl font-bold text-center">404 Not Found</h1>
     <p class="mt-4 text-center">The page you are looking for does not exist.</p>
+
+    <RouterLink
+      to="/"
+      class="mt-8 px-4 py-2 text-white bg-twc-theme-600 hover:bg-twc-theme-700 duration-200 rounded-md"
+    >
+      Go Home
+    </RouterLink>
   </section>
 </template>
