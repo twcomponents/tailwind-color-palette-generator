f345d0f9aabe73320508c25f2fd7f01f3203c191
Eyüp <epsilonelittr@gmail.com>
2024-11-28 15:48:16 +0300

    Add some comments

--- src/shared/utils/export.util.ts
+++ src/shared/utils/export.util.ts
@@ -2,6 +2,13 @@ import { IPaletteColor } from '../models/color.model';
 import _ from 'lodash';
 
 export default class ExportUtil {
+  /**
+   * Export the color palette as JSON format.
+   *
+   * @param colorPalette The color palette.
+   * @param paletteName The name of the palette.
+   * @returns The JSON string.
+   */
   public static exportAsJson(
     colorPalette: IPaletteColor[],
     paletteName: string
