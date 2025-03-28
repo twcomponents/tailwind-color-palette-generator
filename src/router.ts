0c9b789c94c1943f79857f315a665721f893d1f6
Eyüp <epsilonelittr@gmail.com>
2025-03-29 02:05:42 +0300

    Rename view components

--- src/router.ts
+++ src/router.ts
@@ -1,24 +1,24 @@
 import { createWebHistory, createRouter } from 'vue-router';
 
 // views
-import Home from './views/Home.vue';
-import NotFound from './views/NotFound.vue';
+import HomeView from './views/HomeView.vue';
+import NotFoundView from './views/NotFoundView.vue';
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     // home
-    { name: 'home', path: '/', component: Home },
+    { name: 'home', path: '/', component: HomeView },
     // handles /:primary/:secondary?
     {
       name: 'home-static',
       path: '/:primary([a-fA-F0-9]{6})/:secondary([a-fA-F0-9]{6})?',
-      component: Home,
+      component: HomeView,
     },
     // 404
     {
       path: '/:pathMatch(.*)*',
-      component: NotFound,
+      component: NotFoundView,
     },
   ],
 });
